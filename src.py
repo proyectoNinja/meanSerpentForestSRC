de62403c156bfda6a78d122e0caa44e1e47fdbad
tfg <tfg@debian>
2017-03-10 20:00:23 +0100

    superficial modification, commented instructions for solving the current problem making 4-hours group

--- src.py
+++ src.py
@@ -3,7 +3,13 @@ import time
 import datetime
 
 print "Leyendo archivo..."
-data=pd.read_table('../csv.txt',header = 1, usecols=[1,2,3,4])
+columnas=['Hora','Tipo','Historico','Leida']
+data=pd.read_table('../csv.txt',header = 1, usecols=[1,2,3,4],names=columnas,parse_dates=True)
+#data['Time']=data['Hora'].map(lambda x: pd.to_timeStamp()
+#print data
+#print data[(data['Hora']> '' )]
+pd.groupby(data, by=[data['Hora']<'2016/04/9'])
+pd.groupby(data, by=[data['Hora']>'2016/04/13 12:00'])
+#data.groupby(pd.TimeGrouper(freq='H'))
 print data
-
 print "Hecho"
