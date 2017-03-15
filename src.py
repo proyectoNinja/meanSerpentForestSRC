d5c664e184a5c396bf29f5e859bdeb13ecfb2cba
usuario <usuario@debian>
2017-03-15 14:54:24 +0100

    function clasifiPor hora: recieves two dates as strings dateToClassify and dateRef and returns a value from 1 to 90 representing the 6 hour group in which dateToClassify should be clustered.

--- src.py
+++ src.py
@@ -1,27 +1,30 @@
 import pandas as pd
 import time
-import datetime
+from datetime import datetime,timedelta
 
 def clasificaPorHora(hora,hMin):
+    format="%Y/%m/%d %H:%M"
+    hClasificar=datetime.strptime(hora,format)
+    hRef=datetime.strptime(hMin,format)
     grupo=0
     clasificado=False
-    while(grupo<90&&!clasificado):
+    while(grupo<90 and not clasificado):
         horas=4*(grupo+1)
-        if(hora+timedelta(hours=horas)>hora):
+        if(hRef+timedelta(hours=horas)>hClasificar):
             clasificado=True
         else:
             grupo=grupo+1
         return grupo
-        
-print "Leyendo archivo..."
-columnas=['Hora','Tipo','Historico','Leida']
-data=pd.read_table('../csv.txt',header = 1, usecols=[1,2,3,4],names=columnas,parse_dates=True)
+
+#print "Leyendo archivo..."
+#columnas=['Hora','Tipo','Historico','Leida']
+#data=pd.read_table('../csv.txt',header = 1, usecols=[1,2,3,4],names=columnas,parse_dates=True)
 
 #data['Time']=data['Hora'].map(lambda x: pd.to_timeStamp()
 #print data
 #print data[(data['Hora']> '' )]
-pd.groupby(data, by=[data['Hora']<'2016/04/9'])
-pd.groupby(data, by=[data['Hora']>'2016/04/13 12:00'])
+#pd.groupby(data, by=[data['Hora']<'2016/04/9'])
+#pd.groupby(data, by=[data['Hora']>'2016/04/13 12:00'])
 #data.groupby(pd.TimeGrouper(freq='H'))
-print data
+print clasificaPorHora("2016/03/30 16:44","2016/03/30 16:31")
 print "Hecho"
