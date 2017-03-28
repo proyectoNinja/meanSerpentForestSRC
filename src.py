620865601290cbac087b1422a17e1b8500b99e12
tfg <tfg@debian>
2017-03-28 20:55:36 +0200

    parser in function with variable file feature and filtro update with unfinished type 4 lecture

--- src.py
+++ src.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import time
+import sys
 from datetime import datetime,timedelta
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
@@ -13,18 +13,25 @@ from sklearn.cross_validation import KFold
 
 def filtro(data,tipo):
     if (tipo==0):
-        historico=data.drop('Leida',axis=1)
-        historico=historico.drop('Tipo',axis=1)
-        historico=historico.dropna(axis=0)
-        final=historico.reindex()
-        final.set_index('Hora', inplace=True)
+        col=['Historico','Hora','Grupo']
+        data=data[col]
+        data=data.dropna(axis=0)
+        data.set_index('Hora', inplace=True)
     elif (tipo==1):
-        leida=data.drop('Historico',axis=1)
-        leida=leida.drop('Tipo',axis=1)
-        leida=leida.dropna(axis=0)
-        final=leida.reindex()
-        final.set_index('Hora', inplace=True)
-    return final
+        col=['Historico','Hora','Grupo']
+        data=data[col]
+        data=data.dropna(axis=0)
+        data.set_index('Hora', inplace=True)
+    elif (tipo==5):
+        col=['Carbohidratos','Hora','Grupo']
+        data=data[col]
+        data=data.dropna(axis=0)
+        data.set_index('Hora', inplace=True)
+    elif (tipo==4):
+        #data['InsulinaTiempo']=Rapida/lenta 1/0
+        #data['InsulinaCantidad']=Int/NaN
+        print 4
+    return data
 
 def clasificaPorHora(hora,hMin,format):
     hClasificar=datetime.strptime(hora,format)
@@ -48,24 +55,26 @@ def printAndPlotGroup(data,tipo,grupo):
 
 def parser(dir):
     columnas=['Hora','Tipo','Historico','Leida','Insulina rapida SV','Insulina rapida U','Alimentos SV','Carbohidratos','Insulina lenta SV']
-    df=pd.read_table(dir,header=1,usecols=[1,2,3,4,5,6,7,8,9],names=columnas,parse_dates='Hora')
+    data=pd.read_table(dir,header=1,usecols=[1,2,3,4,5,6,7,8,9],names=columnas,parse_dates='Hora')
+    format="%Y/%m/%d %H:%M"
+    primeraHora = data['Hora'].min()
+    data['Grupo']=data['Hora'].map(lambda x: clasificaPorHora(x,primeraHora,format))
+    return data
 
-print "Leyendo archivo..."
-columnas=['Hora','Tipo','Historico','Leida']
-data=pd.read_table('../csv.txt',header = 1, usecols=[1,2,3,4],names=columnas,parse_dates='Hora')
-
-print "Realizando adaptaciones pertinentes..."
-print "Esto puede tardar unos segundos"
-format="%Y/%m/%d %H:%M"
-data['Grupo']=data['Hora'].map(lambda x: clasificaPorHora(x,"2016/03/30 16:30",format))
+if (len(sys.argv)==2):
+    archivo=sys.argv[1]
+else:
+    archivo="../csv.txt"
+    print "archivo ejemplo"
+data=parser(archivo)
 data=filtro(data,0)
 data_agrupada=data.groupby('Grupo')
 grupos=[]
 for index,grupo in data_agrupada:
     grupo=grupo.drop('Grupo',axis=1)
     grupos.append(grupo)
-#print grupos[3]
+print grupos[66]
+grupos=np.array(grupos, dtype=object)
 cluster=KMeans()
-
-
+#cluster.fit(data)"""
 print "Todo ha salido a pedir de boca"
