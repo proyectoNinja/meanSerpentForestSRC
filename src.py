0cffe601d8b4cddaddcd477788b95dd388e1a768
tfg <tfg@debian>
2017-03-30 16:53:32 +0200

    data prepared for clustering droping all groups that haven't got exactly 16 rows

--- src.py
+++ src.py
@@ -39,16 +39,10 @@ def filtro(data,tipo):
             #data['InsulinaCantidad']=Int/NaN
     """
     format="%Y/%m/%d %H:%M"
-    data['Indice']=data['Hora'].map(lambda hora: int(time.mktime(datetime.strptime(hora,format).timetuple())))
-    data=data.drop('Hora',axis=1)
-    data.set_index('Indice', inplace=True)
+    #data['Indice']=data['Hora'].map(lambda hora: int(time.mktime(datetime.strptime(hora,format).timetuple())))
+    #data=data.drop('Hora',axis=1)
+    data.set_index('Hora', inplace=True)
     return data
-
-def estimador(comida,data):
-    if (comida>0):
-        return data['Carbohidratos'].median()
-    else:
-        return 0
 
 def clasificaPorHora(hora,hMin,format):
     hClasificar=datetime.strptime(hora,format)
@@ -63,38 +57,49 @@ def clasificaPorHora(hora,hMin,format):
             grupo=grupo+1
     return int(grupo)
 
-def printAndPlotGroup(data,tipo,grupo):
-    imprimir=filtro(data.get_group(grupo),tipo)
-    imprimir=imprimir.drop('Grupo',axis=1)
+def printAndPlotGroup(data,grupo):
+    #imprimir=filtro(data.get_group(grupo),tipo)
+    imprimir=data.get_group(grupo).drop('Grupo',axis=1)
     print imprimir
     imprimir.plot()
     plt.show()
 
 def parser(dir):
-    columnas=['Hora','Tipo','Historico','Leida','Insulina rapida SV','Insulina rapida U','Alimentos SV','Carbohidratos','Insulina lenta SV']
-    data=pd.read_table(dir,header=1,usecols=[1,2,3,4,5,6,7,8,9],names=columnas,parse_dates='Hora')
+    columnas=['Hora','Tipo','Historico','Leida','Insulina rapida SV',
+    'Insulina rapida U','Alimentos SV','Carbohidratos','Insulina lenta SV']
+    data=pd.read_table(dir,header=1,usecols=[1,2,3,4,5,6,7,8,9],
+    names=columnas,parse_dates='Hora')
     format="%Y/%m/%d %H:%M"
     primeraHora = data['Hora'].min()
     data['Grupo']=data['Hora'].map(lambda x: clasificaPorHora(x,primeraHora,format))
     return data
 
+def agrupar(data):
+    contadorDeApariciones = data['Grupo'].value_counts(sort=False)
+    grupo=0
+    for a in contadorDeApariciones:#esto es muy ineficiente
+        if (a!=16):
+            data=data[data.Grupo!=grupo]
+        grupo=grupo+1
+    data_agrupada=data.groupby('Grupo')
+    return data_agrupada
 
-if (len(sys.argv)>1):
-    archivo=sys.argv[1]
-else:
-    archivo="../csv.txt"
-    print "archivo ejemplo"
-data=parser(archivo)
-data=filtro(data,0)
-data_agrupada=data.groupby('Grupo')
-"""drop de 'grupo' y clustering
-    Esto deberia dropear la columa grupos pero no lo hace
-    for index,grupo in data_agrupada:
+def main():
+    if (len(sys.argv)>1):
+        archivo=sys.argv[1]
+    else:
+        archivo="../csv.txt"
+    data=parser(archivo)
+    data=filtro(data,0)
+    data=agrupar(data)
+    data_agrupada=[]
+    for index,grupo in data:
         grupo=grupo.drop('Grupo',axis=1)
-        print index
-        print grupo
-    print data_agrupada.get_group(82)
+        if (index==0):
+            print grupo
+        data_agrupada.append(grupo['Historico'])
     cluster=KMeans()
     cluster.fit(data_agrupada)
-"""
-print "Todo ha salido a pedir de boca"
+    print "Todo ha salido a pedir de boca"
+
+main()
