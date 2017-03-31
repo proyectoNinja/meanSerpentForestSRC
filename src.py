d94cd1680547683b4002ddb1298099e617cfee4c
tfg <tfg@debian>
2017-03-31 18:36:32 +0200

    get clusters code, no plotting yet

--- src.py
+++ src.py
@@ -5,11 +5,10 @@ import time
 import timeit
 from datetime import datetime,timedelta
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm
 from sklearn.cluster import KMeans
-from sklearn.utils import check_random_state, shuffle
 
 valorMinimo=8
+_nClusters=8
 
 def filtro(data,tipo):
     if (tipo==0):
@@ -78,7 +77,7 @@ def parser(dir):
     print end - timeit.timeit()
     return data
 
-def agrupar(data):
+def cribador(data):
     contadorDeApariciones = data['Grupo'].value_counts(sort=False)
     grupo=0
     for a in contadorDeApariciones:#esto es muy ineficiente
@@ -87,82 +86,38 @@ def agrupar(data):
         elif(a>=valorMinimo and a<16):
             rowsNeeded=16-a
         grupo=grupo+1
-    data_agrupada=data.groupby('Grupo')
     #print data.describe()
-    return data_agrupada
-
-def hazAlgoConEsto(clustering):#BSD http://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_stability_low_dim_dense.html#sphx-glr-auto-examples-cluster-plot-kmeans-stability-low-dim-dense-py
-    random_state = np.random.RandomState(0)
-
-    # Number of run (with randomly generated dataset) for each strategy so as
-    # to be able to compute an estimate of the standard deviation
-    n_runs = 5
-
-    # k-means models can do several random inits so as to be able to trade
-    # CPU time for convergence robustness
-    n_init_range = np.array([1, 5, 10, 15, 20])
-
-    # Datasets generation parameters
-    n_samples_per_center = 100
-    grid_size = 3
-    scale = 0.1
-    n_clusters = grid_size ** 2
+    return data
 
 
-    def make_data(random_state, n_samples_per_center, grid_size, scale):
-        random_state = check_random_state(random_state)
-        centers = np.array([[i, j]
-                            for i in range(grid_size)
-                            for j in range(grid_size)])
-        n_clusters_true, n_features = centers.shape
+def procesado(data):
+    data=cribador(data)
+    data_final=data.groupby('Grupo')
+    data_agrupada=[]
+    for index,grupo in data_final:
+        data_agrupada.append(grupo['Historico'])
+        #data_tratable=data.drop(values=index,axis=0)
+    clustering=KMeans(n_clusters=_nClusters)
+    clustering.fit(data_agrupada)
+    #data['Cluster']=data.['grupo'].map(lambda x: clustering.labels_[x])
 
-        noise = random_state.normal(
-            scale=scale, size=(n_samples_per_center, centers.shape[1]))
+    clusters=[]
+    for i in range(_nClusters):
+        clusters.append([])
+    for i,j in zip(clustering.labels_,data_agrupada):
+        clusters[i].append(j)
+    for i in range(_nClusters):
+        plt.plot(clusters[i])
+        plt.show()
 
-        X = np.concatenate([c + noise for c in centers])
-        y = np.concatenate([[i] * n_samples_per_center
-                            for i in range(n_clusters_true)])
-        return shuffle(X, y, random_state=random_state)
-    X, y = make_data(random_state, n_samples_per_center, grid_size, scale)
-    fig = plt.figure()
-    for k in range(8):
-        my_members = clustering.labels_ == k
-        color = cm.spectral(float(k) / 8, 1)
-        plt.plot(X[my_members, 0], X[my_members, 1], 'o', marker='.', c=color)
-        cluster_center = clustering.cluster_centers_[k]
-        plt.plot(cluster_center[0], cluster_center[1], 'o',
-                 markerfacecolor=color, markeredgecolor='k', markersize=6)
-        plt.title("Example cluster allocation with a single random init\n"
-                  "with MiniBatchKMeans")
-    plt.show()
-
-def clustering(clustering):
-    a=[]
 
 def main():
-    start = timeit.timeit()
-    end = timeit.timeit()
-    print "tiempo inicial"
-    print end - start
     if (len(sys.argv)>1):
         archivo=sys.argv[1]
     else:
         archivo="../csv.txt"
     data=parser(archivo)
     data=filtro(data,0)
-    print "entre filtro y agrupar"
-    print timeit.timeit() - start
-    data=agrupar(data)
-    print "entre agrupar y convertir para KMeans"
-    print timeit.timeit() - start
-    data_agrupada=[]
-    for index,grupo in data:
-        data_agrupada.append(grupo['Historico'])
-    print "convertidp para KMeans"
-    print timeit.timeit() - start
-    clustering=KMeans()
-    clustering.fit(data_agrupada)
-    #hazAlgoConEsto(clustering)
-    print "Todo ha salido a pedir de boca"
+    procesado(data)
 
 main()
