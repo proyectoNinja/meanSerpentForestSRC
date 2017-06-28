19dfd49f5e6941d98cb2d6bea4b75161948ea7aa
Max Lange <fegatello@debian>
2017-06-28 16:45:44 +0200

    latex file

--- persistenceFile.py
+++ persistenceFile.py
@@ -44,8 +44,6 @@ def getPlotAndSave(clusters,ruta,metodo,nombreArchivo):
             plt.plot(group)
             plt.axis([0,15,40,350])
     plt.savefig(ruta+nombreArchivo+'_'+metodo+'_'+str(n_clusters)+'_img.png')
-    #plt.show()
-    #return plt
 
 def genTablaOrigin(code,pdf):
     pdf.cell(10,8,'','LTBR',0,'C',0)
@@ -210,6 +208,86 @@ def saveData(ruta,etiquetas,nombres,datos,metodo,nombreArchivo=""):
         for file in files:
             fantasy_zip.write(os.path.join(root, file))
     fantasy_zip.close()
+
+def genLatex(clusters,code,metodo):
+    n=0
+    tra=0
+    for tramos in clusters:
+        n+=1
+        for tramo in tramos:
+            tra+=1
+    frase=""
+    frase+='\section{Informe gluc\'{e}mico}\n Aplicando una serie de t\'{e}cnicas basadas en el aprendizaje autom\'{a}tico,'
+    frase+='se realiza un estudio sobre los patrones que reflejan sus niveles de glucosa en tramos de cuatro horas, comenzando desde las 00:00h. Cada fragmento pertenece por tanto a una franja horario:\n'
+	frase+="\\begin{itemize}\n \item Franja 0 : de medianoche (00:00h) a las 04:00h\n \item Franja 1 : de 04:00h a las 08:00h\n"
+    frase+="\item Franja 2 : de 00.08h a medio\'{i}a (12:00h) \n \item Franja 3 : de  medio\'{i}a (12:00h) a las 16:00h \n \item Franja 4 : de 16:00h a las 20:00h \n\item Franja 5 : de las 20:00h  a medianoche (00:00h)\n \end{itemize}\n"
+	frase+='En este caso se ha optado por la t\'{e}cnica de clustering conocida como \textit{'+ metodo
+    frase+=", tras haber identificado hasta "+ str(tra)
+    frase+=" tramos v\'{a}lidos y completos, los hemos asociado en " str(n)
+    frase+=" grupos o clusters. \n"
+    if (metodo=='hdbscan'):
+        frase+=" N\'{o}tese que el \'{u}ltimo de los clusters correspodiente al ruido, es decir, no compone un cluster en si mismo."
+
+    frase+='Cada una de las \'{i}neas que ver\'{a}en las gr\'{a}ficas representa un fragmento de 4 horas distinto. Cada uno de estos clusters puede caracterizarse seg\'{u}n unas m\'{e}tricas estad\'{i}sticas que se muestran en la tabla \ref{tab:param}.'
+	frase+= 'De cada cluster (enumerados de 1 en adelante, de izquierda a derecha y de arriba a abajo) detallaremos a la cantidad de fragmentos de cada tramo horario que contiene y los caracterizaremos por un conjunto de m\'{e}tricas estad\'{i}sticas.\n'
+	frase+='\subsection{Descripci\'{o}n de las agrupaciones}\n La tabla \ref{tab:clusterdescription} muestra la frecuencia de cada tramo horario en cada uno de los clusters. Esta tabla se puede resumir en: \begin{enumerate}\n'
+    for cluster,nCluster in zip(code,range(len(code))):
+        frase+="\item El cluster n\'{u}mero "+ str(nCluster+1)+" esta formado por "
+        salto=False
+        for tramo, nTramo in zip(cluster,range(len(cluster))):
+            if(tramo>0):
+                salto=True;
+                if(tramo==1):
+                    frase+= "1 tramo correspodiente "
+                elif(tramo>1):
+                    frase+= str(tramo)+" tramos correspondientes "
+                frase+="a las horas entre "
+                if(nTramo==0):
+                    frase+="0h y las 4 am, "
+                elif(nTramo==1):
+                    frase+="las 4 am y las 8 am, "
+                elif(nTramo==2):
+                    frase+="las 8 am y 12am, "
+                elif(nTramo==3):
+                    frase+="12 y las 4 pm, "
+                elif(nTramo==4):
+                    frase+="las 4 pm y las 8 pm, "
+                elif(nTramo==5):
+                    frase+="las 8 pm y medianoche."
+        if (salto):
+            frase+='\n'
+	frase+='\end{enumerate}\n \begin{table}[ht]\n \caption{Composici\'{o}n de los clusters obtenidos mediante \textit{'+ metodo +'}. N\'{u}mero de los tramos de cada franja horaria que contiene cada cluster}\n \centering\n \begin{tabular}{|c|c|c|c|c|c|c|}\n \hline\n Cluster & Franja 0 & Franja 1& Franja 2& Franja 3& Franja 4& Franja 5 \n \hline'
+    for cluster in code:
+        frase+=str(n)+'&'
+        for tramo,nTramo in zip(cluster,range(len(cluster))):
+            if (nTramo<len(cluster)-1):
+				frase+=str(tramo)+'&'
+			else:
+				frase+=str(tramo)+'//\n'
+        n+=1
+
+	frase+='\hline \n\end{tabular}\n\label{tab:clusterdescription}\n\end{table}\n La caracterizaci\'{o}n de los clusters se presenta en la tabla \ref{tab:carac} atendiendo a las siguientes variables \n \begin{itemize}\n \item (A) - Glucosa Media\n \item (B) - Desviación típica\n\item (C) - Glucosa máxima media'
+	frase+='\n\item (D) - Glucosa mínima media\n\item (E) - Porcentaje de tiempo en rango 70-180\n\item (F)- Número medio de eventos por debajo del mínimo(60)\n \item (G) - Número medio de eventos por encima del máximo (240)\n \item (H) - Máximo número de eventos fuera de rango(60-240)\n \item (I) - Mínimo número de eventos fuera de rango(60-240)\n\end{itemize}\n'
+	frase+='\begin{table}[]\n \centering \n\caption{Caracterizaci\'{o}n de los clusters obtenidos mediante \textit{K-means}.}\n\label{tab:carac}\n\begin{tabular}{|llllllllll|}\n\hline \n &A & B      & C     & D      & E      & F       & G    & H    & I     \\\n\hline'
+	for cluster in info:
+        for dato,d in zip(cluster,range(len(cluster))):
+            if (d<4 or d==5 or d==6):
+                frase+=str(round(dato,2))+' & '
+            elif(d==4):
+                frase+=str(round(dato,2))+' \% & '
+            elif(d==7):
+                frase+=str(int(dato))+' & '
+			elif(d==8):
+                frase+=str(int(dato))+' \\ \n '
+
+	frase+='\hline\n \end{tabular}\n\end{table}\n'
+    return frase
+
+def toLatex(clusters, code, metodo,ruta=""):
+    file=open(ruta+"file.tex","w")
+    file.write(genLatex(clusters,code,metodo))
+    file.close()
+
 
 def toPDF(clusters,codes,metodo,ruta="",nombreArchivo=""):
     nucleos=len(clusters)
